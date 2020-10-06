5122edd241c2857b540fda472157104ee16ea274
Vinicius F <vinilima07@gmail.com>
2020-10-05 22:31:33 -0300

    Revision - fix issues

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy
 import math
+import sys
 
 class NeuralNetwork:
     def __init__(self, d_input, d_output, lr=0.1, max_it=100, activateFunc="step"):
@@ -17,6 +18,8 @@ class NeuralNetwork:
 
     # train procedure
     def train(self):
+        print("Treinando...")
+
         ERRO = 1
         it = 0
         while it < self.max_it and ERRO > 0.1:
@@ -33,14 +36,16 @@ class NeuralNetwork:
         print(self.weights)
         _isTrained = True
 
-    
-    # test the value passed
+    # Avalia o valor passado
+
     def evaluate(self, x, print_=False):
         wTimesXPlusB = self.weights.dot(x) + self.bias
         if print_:
+            print("\nAvaliação da entrada")
             print(wTimesXPlusB)
         return self.activateFunc(wTimesXPlusB)
 
+    # Eleva ao quadrado todos os elementos da matriz
     def square(self, matrix):
         resp = matrix
         for r, row in enumerate(resp):
@@ -48,15 +53,37 @@ class NeuralNetwork:
                 resp[r][c] = column**2
         return resp
 
-    def sigmoid(self, value):
-        resp = value
+    # Função de Ativação Sigmoidal
+    def sigmoid(self, matrix):
+        resp = matrix
         for r, row in enumerate(resp):
-            for c, column in enumerate(resp):
+            for c, column in enumerate(row):
                 resp[r][c] = 1 / (1 + math.exp(-column))
+
+        return self.normalizeByGreater(resp)
+
+    # Normaliza a matriz resultado, transformando o maior valor
+    # dentre os elementos o unico não nulo
+    def normalizeByGreater(self, matrix):
+        resp = matrix
+        greater = (0, 0)
+        for r, row in enumerate(resp):
+            for c, column in enumerate(row):
+                if column > resp[greater[0]][greater[1]]:
+                    greater = (r, c)
+
+        for r, row in enumerate(resp):
+            for c, column in enumerate(row):
+                if (r, c) != greater:
+                    resp[r][c] = 0
+                else:
+                    resp[r][c] = 1
+
         return resp
- 
-    def step(self, value):
-        resp = value
+
+    # Função de Ativação Degrau
+    def step(self, matrix):
+        resp = matrix
         for r, row in enumerate(resp):
             for c, column in enumerate(row):
                 if column < 0.5:
@@ -64,10 +91,16 @@ class NeuralNetwork:
                 else:
                     resp[r][c] = 1
         return resp
-        
 
 
 if __name__ == "__main__":
+
+    classification = {
+        "Iris-setosa": [0.0, 0.0, 1.0],
+        "Iris-versicolor": [0.0, 1.0, 0.0],
+        "Iris-virginica": [1.0, 0.0, 0.0]
+    }
+
     data = pd.read_csv('iris.data', header=None)
 
     # amostras
@@ -76,23 +109,36 @@ if __name__ == "__main__":
     # rotulos
     d_output = data[4].to_numpy()
 
-
+    # classifica as saidas de forma matricial
     classes = []
-
-    for classification in d_output:
-        if classification == 'Iris-setosa':
-            classes.append([0.0, 0.0, 1.0])
-        elif classification == 'Iris-versicolor':
-            classes.append([0.0, 1.0, 0.0])
-        elif classification == 'Iris-virginica':
-            classes.append([1.0, 0.0, 0.0])
+    for _class in d_output:
+        classes.append(classification[_class])
 
     classes = numpy.array(classes)
 
-    nn = NeuralNetwork(d_input, classes, max_it=20, activateFunc="sigmoid")
+    to_classify = []
+    print("Insira os valores para a avaliação: num -> [PRESS ENTER]")
+    for i in range(4):
+        n = input("num {}: ".format(i+1))
+        to_classify.append(float(n))
+
+    print("")
+    max_it = int(input("Insira o máximo de iterações: dica(200)"))
+
+    print("")
+    activateFunc = "step"
+    if int(input("Escolha a função de ativação: sigmoid(1) - step(2): ")) == 1:
+        activateFunc = "sigmoid"
+
+    # 6.8,2.8,4.8,1.4
+    nn = NeuralNetwork(
+        d_input, classes, max_it=max_it, activateFunc=activateFunc)
     nn.train()
-    result = nn.evaluate(numpy.asmatrix([5.1,3.5,1.4,0.2]).T, True)
-    
+
+    result = nn.evaluate(numpy.asmatrix(to_classify).T, True)
+
     print("\nFinal Results")
-    print(result)
-    
+    for key, value in classification.items():
+        if (result.T == value).all():
+            print("{} = {}".format(result.T, key))
+            break
